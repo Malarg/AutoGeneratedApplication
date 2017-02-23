c2e446c3ea67fcc8e6f0235228a14d8cf63e38d4
Дмитрий Репин < >
2017-02-23 15:09:10 +0500

    Created ICommunication interface

---
+++ Backend/Interfaces/ICommunication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backend.Models;
+
+namespace Backend.Interfaces
+{
+    interface ICommunication
+    {
+        BaseReporter AddAttribute(string name, string type, string tableName);
+        BaseReporter DeleteAttribute(string name, string tableName);
+        BaseReporter AddTable(string name, Dictionary<string, string> properties);
+        BaseReporter DeleteTable(string name);
+    }
+}
