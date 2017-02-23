cbb7230b5208ccf354d79823019ca52a9128ac09
Дмитрий Репин < >
2017-02-23 14:15:32 +0500

    first commit

---
+++ Infrastructure/DataItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class DataItem
+    {
+        public string TableName { get; set; }
+        public int RecordId { get; set; }
+        public string PropertyName { get; set; }
+        public object Value { get; set; }
+    }
+}
---
+++ Infrastructure/TableStructureItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class TableStructureItem
+    {
+        public string TableName { get; set; }
+        public Dictionary<string, Type> Properties { get; set; }
+    }
+}
